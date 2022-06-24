88e438dcb07808acfe25c56d702c1490c2effc47
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-06-24 09:08:55 +0100

    Update delay_time.hpp

--- plugin/blink/standard_parameters/delay_time.hpp
+++ plugin/blink/standard_parameters/delay_time.hpp
@@ -3,6 +3,7 @@
 #include <blink/parameters/envelope_parameter_spec.hpp>
 #include <blink/parameters/envelope_manipulator_target_spec.hpp>
 #include <blink/parameters/slider_parameter_spec.hpp>
+#include <blink/math.hpp>
 
 namespace blink {
 namespace std_params {
@@ -14,6 +15,8 @@ auto inline slider()
 {
 	SliderSpec<float> out;
 
+	out.default_value = math::convert::delay_time_ms_to_linear(100.0f);
+
 	out.constrain = [](float v)
 	{
 		return std::clamp(v, 0.0f, 1.0f);
@@ -21,8 +24,26 @@ auto inline slider()
 
 	out.decrement = [](float v, bool precise)
 	{
+		v = math::convert::linear_to_delay_time_ms(v);
+		v -= (precise ? 1.0f : 10.0f);
+		v = math::convert::delay_time_ms_to_linear(v);
+
 		return v;
 	};
+
+	out.drag = [](float v, int amount, bool precise)
+	{
+		v = math::convert::linear_to_delay_time_ms(v);
+		v = tweak::drag<float, 100, 1000>(v, amount / 5, precise);
+		v = math::convert::delay_time_ms_to_linear(v);
+
+		return v;
+	};
+
+	out.from_string = [](const std::string& str)
+	{
+	};
+
 
 	return out;
 }
