23faf132f6dbde2e3428e121194c17db65424c62
Colugo <colugomusic@gmail.com>
2024-02-03 20:55:39 +0000

    just cleanup

--- plugin/blink/transform/calculators/pitch.hpp
+++ plugin/blink/transform/calculators/pitch.hpp
@@ -41,16 +41,26 @@ template <class T> T weird_math_ff(T min, T max, T distance, T n) {
 	return math::convert::p_to_ff(min) * std::pow(ratio(min, max, distance), n);
 }
 
+struct PitchPoint {
+	blink_IntPosition x;
+	float pitch; 
+	float ff;
+}; 
 
-class PitchUnit
-{
-public:
+inline
+auto make_pitch_point(const blink_FloatPoint& p, float min, float max, float transpose) {
+	PitchPoint pp;
+	pp.x     = p.x;
+	pp.pitch = std::clamp(p.y, min, max) + transpose;
+	pp.ff    = math::convert::p_to_ff(pp.pitch);
+	return pp;
+}
 
+struct PitchUnit {
 	struct Config {
-		float transpose { 0.0f };
-		const blink_EnvelopeData* pitch { nullptr };
+		float transpose                 = 0.0f;
+		const blink_EnvelopeData* pitch = nullptr;
 	};
-
 	// We use this for both sample playback and waveform generation. This
 	// calculation needs to be fast, preferably O(n) or better.
 	//
@@ -58,147 +68,102 @@ public:
 	// millions of pixels off the left edge of the screen therefore it is
 	// not good enough to simply traverse the entire sample.
 	//
-	blink_Position operator()(Config config, blink_Position block_position, float* derivative = nullptr) {
-		struct PitchPoint {
-			blink_IntPosition x;
-			float pitch; 
-			PitchPoint(const blink_FloatPoint& p, float min, float max, float transpose)
-				: x(p.x)
-				, pitch(std::clamp(p.y, min, max) + transpose)
-				, ff_(1.0f)
-			{
-			} 
-			PitchPoint(PitchPoint && rhs) = default; 
-			float get_ff() const {
-				if (!ff_ready_) {
-					ff_ = math::convert::p_to_ff(pitch);
-					ff_ready_ = true;
-				} 
-				return ff_;
-			} 
-		private: 
-			mutable bool ff_ready_ = false;
-			mutable float ff_;
-		}; 
-		for (blink_Index i { point_search_index_ }; i < config.pitch->points.count; i++) {
-			const PitchPoint p1(config.pitch->points.data[i], config.pitch->points.min, config.pitch->points.max, config.transpose); 
+	blink_Position xform(Config config, blink_Position block_position, float* derivative = nullptr) {
+		for (blink_Index i = point_search_index_; i < config.pitch->points.count; i++) {
+			const auto p1 = make_pitch_point(config.pitch->points.data[i], config.pitch->points.min, config.pitch->points.max, config.transpose);
 			if (block_position < p1.x) {
 				if (i == 0) {
 					point_search_index_ = 0; 
-					if (derivative) *derivative = p1.get_ff(); 
-					return (block_position * p1.get_ff()) + segment_start_;
+					if (derivative) {
+						*derivative = p1.ff;
+					}
+					return (block_position * p1.ff) + segment_start_;
 				} 
-				PitchPoint p0(config.pitch->points.data[i - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
-				auto n { block_position - p0.x };
-				auto segment_size { double(p1.x) - p0.x }; 
+				const auto p0     = make_pitch_point(config.pitch->points.data[i - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
+				auto n            = block_position - p0.x;
+				auto segment_size = double(p1.x) - p0.x;
 				if (segment_size > 0.0f) {
 					point_search_index_ = i; 
-					if (derivative) *derivative = float(weird_math_ff(double(p0.pitch), double(p1.pitch), segment_size, n)); 
+					if (derivative) {
+						*derivative = float(weird_math_ff(double(p0.pitch), double(p1.pitch), segment_size, n)); 
+					}
 					return (weird_math(double(p0.pitch), double(p1.pitch), segment_size, n)) + segment_start_;
 				}
 			}
 			else {
 				if (i == 0) {
 					point_search_index_ = 1; 
-					segment_start_ += (p1.x * p1.get_ff()) + segment_start_;
+					segment_start_ += (p1.x * p1.ff) + segment_start_;
 				}
 				else {
 					point_search_index_ = i + 1; 
-					PitchPoint p0(config.pitch->points.data[i - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
+					const auto p0 = make_pitch_point(config.pitch->points.data[i - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
 					auto segment_size { double(p1.x) - p0.x }; 
 					if (segment_size > 0.0f) {
 						segment_start_ = (weird_math(double(p0.pitch), double(p1.pitch), segment_size, segment_size)) + segment_start_;
 					}
 				}
 			}
-		} 
-		PitchPoint p0(config.pitch->points.data[config.pitch->points.count - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
-		auto n { block_position - p0.x }; 
-		if (derivative) *derivative = p0.get_ff(); 
-		return (n * p0.get_ff()) + segment_start_;
-	}
-
+		}
+		const auto p0 = make_pitch_point(config.pitch->points.data[config.pitch->points.count - 1], config.pitch->points.min, config.pitch->points.max, config.transpose); 
+		auto n        = block_position - p0.x;
+		if (derivative) {
+			*derivative = p0.ff; 
+		}
+		return (n * p0.ff) + segment_start_;
+	} 
 	void reset() {
-		segment_start_ = 0.0f;
+		segment_start_      = 0.0f;
 		point_search_index_ = 0;
-	}
-
-private:
-
-	blink_Position segment_start_ { 0.0f };
-	int point_search_index_ { 0 };
+	} 
+private: 
+	blink_Position segment_start_ = 0.0f;
+	int point_search_index_       = 0;
 };
 
-class Pitch
-{
-public:
-
-	struct Config
-	{
+struct Pitch {
+	struct Config {
 		uint64_t unit_state_id;
 		float transpose;
-		const blink_EnvelopeData* pitch;
-
-		struct
-		{
+		const blink_EnvelopeData* pitch; 
+		struct {
 			BlockPositions* positions;
 			ml::DSPVector* derivatives;
 		} outputs;
 	};
-
-	void operator()(Config config, const BlockPositions& block_positions, int count)
-	{
-		if (!config.pitch || config.pitch->points.count < 1)
-		{
-			const auto get_pitch { [&config]()
-			{
-				if (!config.pitch) return 0.0f;
-				
-				const auto min { config.pitch->points.min };
-				const auto max { config.pitch->points.max };
-
+	void xform(Config config, const BlockPositions& block_positions, int count) {
+		if (!config.pitch || config.pitch->points.count < 1) {
+			const auto get_pitch = [&config]() {
+				if (!config.pitch) {
+					return 0.0f; 
+				}
+				const auto min = config.pitch->points.min;
+				const auto max = config.pitch->points.max;
 				return std::clamp(0.0f, min, max);
-			}};
-
-			const auto ff { math::convert::p_to_ff(get_pitch() + config.transpose) };
-
+			};
+			const auto ff             = math::convert::p_to_ff(get_pitch() + config.transpose);
 			*config.outputs.positions = block_positions.positions * ff;
-
-			if (config.outputs.derivatives)
-			{
+			if (config.outputs.derivatives) {
 				*config.outputs.derivatives = ff;
-			}
-
+			} 
 			return;
-		}
-
-		traverser_.generate(config.unit_state_id, block_positions, count);
-
-		const auto& resets { traverser_.get_resets() };
-
+		} 
+		traverser_.generate(config.unit_state_id, block_positions, count); 
+		const auto& resets = traverser_.get_resets();
 		PitchUnit::Config unit_config;
-
 		unit_config.transpose = config.transpose;
-		unit_config.pitch = config.pitch;
-
+		unit_config.pitch     = config.pitch;
 		config.outputs.positions->rotate_prev_pos();
-
-		for (int i = 0; i < count; i++)
-		{
-			if (resets[i] > 0)
-			{
+		for (int i = 0; i < count; i++) {
+			if (resets[i] > 0) {
 				unit_calculator_.reset();
 			}
-
-			const auto out_derivative { config.outputs.derivatives ? &config.outputs.derivatives->getBuffer()[i] : nullptr };
-			const auto position { unit_calculator_( unit_config, block_positions.positions[i], out_derivative) };
-
+			const auto out_derivative = config.outputs.derivatives ? &config.outputs.derivatives->getBuffer()[i] : nullptr;
+			const auto position       = unit_calculator_.xform(unit_config, block_positions.positions[i], out_derivative);
 			config.outputs.positions->positions.set(i, position);
 		}
 	}
-
 private:
-
 	PitchUnit unit_calculator_;
 	Traverser traverser_;
 };
--- plugin/blink/transform/tape.hpp
+++ plugin/blink/transform/tape.hpp
@@ -11,133 +11,97 @@
 namespace blink {
 namespace transform {
 
-class Tape
-{
-public:
-
-	struct Config
-	{
+struct Tape {
+	struct Config {
 		uint64_t unit_state_id;
 		float transpose {};
 		int64_t sample_offset {};
-
-		struct
-		{
+		struct {
 			const blink_EnvelopeData* pitch {};
-		} env;
-
+		} env; 
 		struct
 		{
 			const blink_OptionData* reverse {};
-		} option;
-
+		} option; 
 		const blink_WarpPoints* warp_points {};
-
-		struct
-		{
-			struct
-			{
+		struct {
+			struct {
 				bool pitch {};
 				bool warped {};
-			} derivatives;
-
+			} derivatives; 
 			bool correction_grains {};
 		} outputs;
-	};
-	
-	void operator()(Config config, const BlockPositions& block_positions, int count);
-
+	}; 
+	void xform(Config config, const BlockPositions& block_positions, int count); 
 	auto& get_pitched_positions() const { return stage_.positions.pitched; }
 	auto& get_warped_positions() const { return stage_.positions.warped; }
 	auto& get_reversed_positions() const { return stage_.positions.reversed; }
 	auto& get_pitched_derivatives() const { return stage_.derivatives.pitched; }
 	auto& get_warped_derivatives() const { return stage_.derivatives.warped; }
-	auto& get_correction_grains() const { return stage_.correction_grains; }
-
-private:
-
+	auto& get_correction_grains() const { return stage_.correction_grains; } 
+private: 
 	void apply_pitch(const Config& config, const BlockPositions& block_positions, int count);
 	void apply_sample_offset(const Config& config);
 	void apply_warp(const Config& config, int count);
-	void apply_reverse(const Config& config, int count);
-
-	struct
-	{
-		struct
-		{
+	void apply_reverse(const Config& config, int count); 
+	struct {
+		struct {
 			BlockPositions pitched;
 			BlockPositions warped;
 			BlockPositions reversed;
-		} positions;
-
-		struct
-		{
+		} positions; 
+		struct {
 			ml::DSPVector pitched;
 			ml::DSPVector warped;
-		} derivatives;
-
+		} derivatives; 
 		CorrectionGrains correction_grains;
-	} stage_;
-
-	struct
-	{
+	} stage_; 
+	struct {
 		calculators::Pitch pitch;
 		calculators::Warp warp;
 		calculators::Reverse reverse;
 	} calculators_;
 };
 
-inline void Tape::operator()(Config config, const BlockPositions& block_positions, int count)
-{
+inline void Tape::xform(Config config, const BlockPositions& block_positions, int count) {
 	apply_pitch(config, block_positions, count);
 	apply_sample_offset(config);
 	apply_warp(config, count);
 	apply_reverse(config, count);
 }
 
-inline void Tape::apply_pitch(const Config& config, const BlockPositions& block_positions, int count)
-{
-	calculators::Pitch::Config calculator_config;
-
+inline void Tape::apply_pitch(const Config& config, const BlockPositions& block_positions, int count) {
+	calculators::Pitch::Config calculator_config; 
 	calculator_config.unit_state_id = config.unit_state_id;
 	calculator_config.transpose = config.transpose;
 	calculator_config.pitch = config.env.pitch;
 	calculator_config.outputs.positions = &stage_.positions.pitched;
-	calculator_config.outputs.derivatives = config.outputs.derivatives.pitch ? &stage_.derivatives.pitched : nullptr;
-
-	calculators_.pitch(calculator_config, block_positions, count);
+	calculator_config.outputs.derivatives = config.outputs.derivatives.pitch ? &stage_.derivatives.pitched : nullptr; 
+	calculators_.pitch.xform(calculator_config, block_positions, count);
 }
 
-inline void Tape::apply_sample_offset(const Config& config)
-{
+inline void Tape::apply_sample_offset(const Config& config) {
 	stage_.positions.pitched.positions -= config.sample_offset;
 }
 
-inline void Tape::apply_warp(const Config& config, int count)
-{
-	calculators::Warp::Config calculator_config;
-
+inline void Tape::apply_warp(const Config& config, int count) {
+	calculators::Warp::Config calculator_config; 
 	calculator_config.unit_state_id = config.unit_state_id;
 	calculator_config.warp_points = config.warp_points;
 	calculator_config.outputs.positions = &stage_.positions.warped;
-	calculator_config.outputs.derivatives = config.outputs.derivatives.warped ? &stage_.derivatives.warped : nullptr;
-
+	calculator_config.outputs.derivatives = config.outputs.derivatives.warped ? &stage_.derivatives.warped : nullptr; 
 	calculators_.warp(calculator_config, stage_.positions.pitched, count);
 }
 
-inline void Tape::apply_reverse(const Config& config, int count)
-{
+inline void Tape::apply_reverse(const Config& config, int count) {
 	// These sub-calculators are used to transform reverse
 	// modulation points into "post-warp-space" by applying
 	// the pitch and warp calculations to each point
-	struct
-	{
+	struct {
 		calculators::PitchUnit pitch;
 		calculators::PitchUnit::Config pitch_config {};
-		calculators::WarpUnit warp;
-
-		struct
-		{
+		calculators::WarpUnit warp; 
+		struct {
 			blink_Position pre_pitch { std::numeric_limits<std::int32_t>::max() };
 			blink_Position pre_warp { std::numeric_limits<std::int32_t>::max() };
 		} prev_positions;
@@ -159,7 +123,7 @@ inline void Tape::apply_reverse(const Config& config, int count)
 
 			sub_calculators.prev_positions.pre_pitch = x;
 
-			x = sub_calculators.pitch(sub_calculators.pitch_config, x, derivative);
+			x = sub_calculators.pitch.xform(sub_calculators.pitch_config, x, derivative);
 		}
 		else
 		{
